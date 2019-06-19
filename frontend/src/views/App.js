2da57d5770baf5913632c397e0e8c2e38b1cc30c
Felix <Felix.Tietjen@txs.de>
2019-06-19 15:56:37 +0200

    feat: added a bunch of files from the would-you-rather project

---
+++ frontend/src/views/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+
+// import Navigation from '../components/Navigation'
+import { Root } from '.'
+
+// import { handleInitialData } from '../actions/shared'
+import { theme } from '../helpers/theme'
+
+/**
+ * The main app that contains the entire page.
+ * This also contains the theme provider.
+ *
+ * @class App
+ * @extends {Component}
+ */
+const App = () => {
+    return (
+        <MuiThemeProvider theme={theme}>
+            <CssBaseline />
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Root} />
+                </Switch>
+            </Router>
+        </MuiThemeProvider>
+    )
+}
+
+export default App
