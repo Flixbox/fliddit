d4366ba7f796985f43a86835ba38ec8ada729f5a
Felix <Felix.Tietjen@txs.de>
2019-06-20 13:19:39 +0200

    feat: added categories to store

--- frontend/src/reducers/categories.js
+++ frontend/src/reducers/categories.js
@@ -1,5 +1,11 @@
 export default (state = {}, action) => {
     switch (action.type) {
+        case 'LOAD_SUCCESS':
+            const categories = action.payload.data.categories
+            if (!categories) return state
+            return {
+                ...categories,
+            }
         default:
             return state
     }
