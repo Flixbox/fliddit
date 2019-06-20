f72fed15f1f4888ed4dc3800cb1c7f0648a8891a
Felix <Felix.Tietjen@txs.de>
2019-06-20 16:37:11 +0200

    feat: implemented sorting

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import { Grid } from '@material-ui/core'
+import { Grid, Button, IconButton } from '@material-ui/core'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import { PostCard } from '.'
 
@@ -18,14 +20,38 @@ const Posts = ({ posts }) => {
         return b[field] - a[field]
     }
 
+    const sortClick = field => {
+        setSorting({ field })
+    }
+
+    const count = Object.keys(posts).length
+
     return (
         <Grid container spacing={1}>
-            {Object.keys(posts).length &&
+            <Grid item xs={12}>
+                {count && <SortControl post={posts[Object.keys(posts)[0]]} sortClick={sortClick} />}
+            </Grid>
+            {count &&
                 Object.keys(posts)
                     .sort(postSort)
                     .map(id => <PostCard {...posts[id]} key={id} />)}
         </Grid>
     )
+}
+
+const SortControl = ({ post, sortClick }) => {
+    const fields = Object.keys(post)
+    return (
+        <>
+            {fields.map(field => (
+                <SortButton field={field} sortClick={sortClick} key={field} />
+            ))}
+        </>
+    )
+}
+
+const SortButton = ({ field, sortClick }) => {
+    return <Button onClick={() => sortClick(field)}>{field} </Button>
 }
 
 export default Posts
