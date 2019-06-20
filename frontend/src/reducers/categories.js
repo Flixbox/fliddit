11deee4b15f7dbb8c06fd23bc7248b0b8b027574
Felix <Felix.Tietjen@txs.de>
2019-06-20 10:32:20 +0200

    feat: added categories reducer and default store

---
+++ frontend/src/reducers/categories.js
@@ -0,0 +1,8 @@
+import defaultStore from '../helpers/defaultStore'
+
+export default (state = defaultStore, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
