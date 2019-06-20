8d0792edf4d9d5ef0781702c4322df07a6151dde
Felix <Felix.Tietjen@txs.de>
2019-06-20 09:44:07 +0200

    feat: used material ui app drawer

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Drawer, ListItem, List, ListItemText } from '@material-ui/core'
 
 const Navigation = () => {
-    return <div>Navigation</div>
+    return (
+        <Drawer variant="permanent" open>
+            <List>
+                <NavigationItem title="FLIDDIT" subtitle="A social platform" />
+                <NavigationItem title="My Account" />
+            </List>
+        </Drawer>
+    )
+}
+
+const NavigationItem = ({ title, subtitle }) => {
+    return (
+        <ListItem button>
+            <ListItemText primary={title} secondary={subtitle} />
+        </ListItem>
+    )
 }
 
 export default Navigation
