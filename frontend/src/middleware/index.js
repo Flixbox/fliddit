11f7607b09c89f5ad15b84744928737ced0ddd17
Felix <Felix.Tietjen@txs.de>
2019-06-20 13:12:09 +0200

    fix: fixed the axios http request

--- frontend/src/middleware/index.js
+++ frontend/src/middleware/index.js
@@ -1,16 +1,19 @@
 import thunk from 'redux-thunk'
 import { applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import axios from 'axios'
+import Axios from 'axios'
 import axiosMiddleware from 'redux-axios-middleware'
 
-const server = 'localhost:3001'
+const server = 'http://localhost:3001'
 
-const client = axios.create({
+const client = Axios.create({
     //all axios can be used, shown in axios documentation
     baseURL: `${server}`,
     responseType: 'json',
-    headers: { Authorization: 'A Fliddit user', Accept: 'application/json' },
+    headers: {
+        Authorization: 'A Fliddit user',
+        Accept: 'application/json',
+    },
 })
 
 export default composeWithDevTools(applyMiddleware(thunk, axiosMiddleware(client)))
