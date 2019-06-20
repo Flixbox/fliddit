ab161834d32100fe751b6e2f1d61904c80efbecf
Felix <Felix.Tietjen@txs.de>
2019-06-20 16:22:27 +0200

    feat: implemented sorting algorithm

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid } from '@material-ui/core'
 
 import { PostCard } from '.'
 
 const Posts = ({ posts }) => {
+    const [sorting, setSorting] = useState({
+        direction: 'desc',
+        field: 'timestamp',
+    })
+
+    const postSort = (aId, bId) => {
+        const { direction, field } = sorting
+        const a = posts[aId]
+        const b = posts[bId]
+
+        if (direction === 'asc') return a[field] - b[field]
+        return b[field] - a[field]
+    }
+
     return (
         <Grid container spacing={1}>
             {Object.keys(posts).length &&
-                Object.keys(posts).map(id => <PostCard {...posts[id]} key={id} />)}
+                Object.keys(posts)
+                    .sort(postSort)
+                    .map(id => <PostCard {...posts[id]} key={id} />)}
         </Grid>
     )
 }
