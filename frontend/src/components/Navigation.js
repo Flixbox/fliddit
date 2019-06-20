c02a2353dd1a9183a728ec2b1d7f95f7facc6414
Felix <Felix.Tietjen@txs.de>
2019-06-20 09:59:46 +0200

    feat: added diviers to nav

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Drawer, ListItem, List, ListItemText } from '@material-ui/core'
+import { Drawer, ListItem, List, ListItemText, Divider } from '@material-ui/core'
 
 const Navigation = () => {
     return (
         <Drawer variant="permanent" open>
             <List>
                 <NavigationItem title="FLIDDIT" subtitle="A social platform" />
+                <Divider />
                 <NavigationItem title="My Account" />
+                <Divider />
             </List>
         </Drawer>
     )
