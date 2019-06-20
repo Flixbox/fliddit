a03e6207f710d169d0e0952de67fd591d64863ed
Felix <Felix.Tietjen@txs.de>
2019-06-20 14:23:30 +0200

    feat: added post card

--- frontend/src/components/PostCard.js
+++ frontend/src/components/PostCard.js
@@ -1,5 +1,13 @@
 import React from 'react'
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, Typography, Card, CardContent, CardMedia } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles(theme => ({
+    details: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+}))
 
 const PostCard = ({
     author,
@@ -12,9 +20,26 @@ const PostCard = ({
     title,
     voteScore,
 }) => {
+    const classes = useStyles()
     return (
-        <Grid item>
-            <Typography>{author}</Typography>
+        <Grid item xs={12} md={6}>
+            <Card className={classes.card}>
+                <div className={classes.details}>
+                    <CardContent className={classes.content}>
+                        <Typography component="h5" variant="h5">
+                            Live From Space
+                        </Typography>
+                        <Typography variant="subtitle1" color="textSecondary">
+                            Mac Miller
+                        </Typography>
+                    </CardContent>
+                </div>
+                <CardMedia
+                    className={classes.cover}
+                    image="/static/images/cards/live-from-space.jpg"
+                    title="Live from space album cover"
+                />
+            </Card>
         </Grid>
     )
 }
