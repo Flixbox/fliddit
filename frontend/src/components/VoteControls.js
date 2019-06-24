159bd53521effd4c2d8ff5203cb1c6e09109ccfc
Felix <Felix.Tietjen@txs.de>
2019-06-24 13:53:39 +0200

    feat: added vote buttons

--- frontend/src/components/VoteControls.js
+++ frontend/src/components/VoteControls.js
@@ -1,18 +1,27 @@
 import React from 'react'
-import { Box } from '@material-ui/core'
+import { Box, IconButton } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const useStyles = makeStyles(theme => ({}))
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+}))
 
 const VoteControls = ({ voteScore, upvote, downvote }) => {
     const classes = useStyles()
 
     return (
         <Box className={classes.root}>
-            <FontAwesomeIcon icon="arrow-circle-up" />
-            <FontAwesomeIcon icon="arrow-circle-down" />
+            <IconButton>
+                <FontAwesomeIcon icon="arrow-circle-up" />
+            </IconButton>
+            <IconButton>
+                <FontAwesomeIcon icon="arrow-circle-down" />
+            </IconButton>
         </Box>
     )
 }
