7293a4db8dbf09ffd4a0ba8253c45498ad9f0f6b
Felix <Felix.Tietjen@txs.de>
2019-06-24 10:50:10 +0200

    feat: enhanced comment loading

--- frontend/src/actions/comments.js
+++ frontend/src/actions/comments.js
@@ -3,6 +3,7 @@ export const LOAD_COMMENT_SECTION = 'LOAD_COMMENT_SECTION'
 export function loadCommentSection({ postId }) {
     return {
         type: LOAD_COMMENT_SECTION,
+        postId,
         payload: {
             request: {
                 url: `/posts/${postId}/comments`,
--- frontend/src/reducers/comments.js
+++ frontend/src/reducers/comments.js
@@ -3,7 +3,7 @@ import { LOAD_COMMENT_SECTION } from '../actions/comments'
 export default (state = {}, action) => {
     switch (action.type) {
         case `${LOAD_COMMENT_SECTION}_SUCCESS`:
-            const comments = action.payload.data.comments
+            const comments = action.payload.data
             if (!comments) return {}
             return {
                 ...comments,
